6bd498db004631eaa8e2fdbff70ff0ef55023a9b
hoesel15 <jvhoesel@live.com>
2024-08-18 19:17:38 -0300

    Final Commit

--- src/main/java/app/entity/Proprietario.java
+++ src/main/java/app/entity/Proprietario.java
@@ -1,43 +1,35 @@
 package app.entity;
 
+import java.util.List;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
-import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity
-@NoArgsConstructor
 
-public class Carro {
+public class Proprietario {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String nome;
-	private int ano;
+	private int idade;
+	
 	@ManyToOne(cascade = CascadeType.ALL)
 	private Marca marca;
 	
-	public String getNome() {
-		return nome;
-	}
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	public int getAno() {
-		return ano;
-	}
-	public void setAno(int ano) {
-		this.ano = ano;
-	}
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "carro_proprietario")
+	private List<Proprietario> proprietarios;
 
-	public long setId(long id) {
-		return id;
-			
 	}
-	
-}
--- src/main/java/app/service/CarroService.java
+++ src/main/java/app/service/CarroService.java
@@ -1,9 +1,12 @@
 package app.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import app.entity.Carro;
+import app.entity.Marca;
 import app.repository.CarroRepository;
 
 @Service
@@ -15,5 +18,45 @@ public class CarroService {
 	public String save(Carro carro) {
 		this.carroRepository.save(carro);
 		return "Carro salvo com sucesso!";		
+	}
+	
+	public String update(Carro carro, long id) {
+		carro.setId(id);
+		this.carroRepository.save(carro);
+				return "Carro foi atualizado com sucesso!";
+		
+	}
+	
+	public String delete (long id) {
+		this.carroRepository.deleteById(id);
+		return "Carro deletado com sucesso!";
+	}
+	
+	public List<Carro> findAll(){
+		List<Carro> lista = this.carroRepository.findAll();
+		return lista;
+	}
+	
+	public Carro findById (long id) {
+		Carro carro = this.carroRepository.findById(id).get();
+		return carro;
+	}
+
+	public List <Carro> findByNome(String nome){
+		return this.carroRepository.findByNome(nome);
+	}
+
+	public List<Carro> listAll() {
+		return null;
+	}
+	
+	public List <Carro> findByMarca(long idMarca){
+		Marca marca = new Marca();
+		marca.setId(idMarca);
+		return this.carroRepository.findByMarca(marca);
+	}
+	
+	public List <Carro> findAcimaAno(int ano){
+		return this.carroRepository.findAcimaAno(ano);
 	}
 }
